401ab45b3bc7eea53036646edfa96dc7ba284928
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-05-03 11:17:38 +0300

    feat: integrate router into main application setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import './assets/style.css'
 import { createPinia } from 'pinia'
+import router from './routers'
 
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(router)
 app.mount('#app')

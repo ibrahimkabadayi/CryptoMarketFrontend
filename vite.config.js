6e71c53079d0d3be56593a79d4e68e21bdeeebb3
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-05-03 11:01:19 +0300

    Added tailwindcss package

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
+// vite.config.js
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
+      // Maps the '@' symbol to the 'src' directory
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })

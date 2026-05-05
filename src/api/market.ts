9218d928b92419da9af0b2be869e71e7a2d00d81
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-05-05 09:52:21 +0300

    feat(api): update market API endpoint and adjust base URL

--- src/api/market.ts
+++ src/api/market.ts
@@ -3,7 +3,7 @@ import type {Coin} from '@/types/market';
 
 export const marketApi = {
     getAllCoins: async () => {
-        const response = await apiClient.get<Coin[]>('/api/Market');
+        const response = await apiClient.get<Coin[]>('/api/market');
         return response.data as Coin[];
     }
 }

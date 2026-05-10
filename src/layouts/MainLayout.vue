bf5cfa919cf909326f34c03ec52e89203c9d133c
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-05-10 21:16:40 +0300

    feat(layout): add sticky NavBar with gradient background

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,8 +3,10 @@ import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-900">
-    <NavBar />
+  <div class="min-h-screen gradient-bg">
+    <div class="sticky top-0 z-50">
+      <NavBar />
+    </div>
 
     <main>
       <router-view></router-view>

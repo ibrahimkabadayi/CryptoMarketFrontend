cf870e6b2ac7a51319a5545ce551390628ad70cf
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-05-21 16:19:12 +0300

    feat(api): extend portfolio API with deposit, withdraw, buyCoin, and sellCoin methods

--- src/api/portfolio.ts
+++ src/api/portfolio.ts
@@ -1,9 +1,9 @@
 import apiClient from "@/api/axios";
-import type { Dashboard } from "@/types/portfolio";
+import type {Dashboard} from "@/types/portfolio";
+import {Coin} from "@/types/market";
 
 export const portfolioApi = {
-    getDashboard: async() =>
-    {
+    getDashboard: async() => {
         const response = await apiClient.get<any>("/api/wallets");
 
         const data = response.data.result;
@@ -16,5 +16,55 @@ export const portfolioApi = {
             assets: data.assets,
             recentTransactions: data.recentTransactions
         } as Dashboard;
-    }
+    },
+
+    deposit: async (walletId: string, amount: number) => {
+        try {
+            const request = {
+                amount: amount
+            };
+
+            const response = await apiClient.post(`api/wallets/${walletId}/transaction`, request, getFinancialHeaders());
+            return response.data;
+        }catch (error) {
+            console.error(error);
+        }
+    },
+
+    withdraw: async (walletId: string, amount: number) => {
+        try {
+            const request = {
+                amount: amount
+            };
+
+            const response = await apiClient.patch(`api/wallets/${walletId}` , request, getFinancialHeaders());
+            return response.data;
+        }catch (error) {
+            console.error(error);
+        }
+    },
+
+    buyCoin: async (walletId: string, quantity: number, coin: Coin) => {
+        const request = {
+            amount: quantity,
+            buyingPrice: coin.currentPrice
+        }
+
+        await apiClient.post(`/api/wallets/${walletId}/assets/${coin.symbol}`, request, getFinancialHeaders());
+    },
+
+    sellCoin: async (walletId: string, amount: number, coin: Coin) => {
+        const request = {
+            amount: amount,
+            price: coin.currentPrice
+        }
+
+        await apiClient.patch(`/api/wallets/${walletId}/assets/${coin.symbol}`, request, getFinancialHeaders());
+    },
 }
+
+const getFinancialHeaders = () => ({
+    headers: {
+        'Idempotency-Key': crypto.randomUUID()
+    }
+});

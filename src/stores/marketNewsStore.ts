c8c6096c74e017c7a80bff5834e97fb5069140c9
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-05-27 20:08:51 +0300

    feat(marketNews): add marketNewsStore with news fetching and real-time updates via SignalR

---
+++ src/stores/marketNewsStore.ts
@@ -0,0 +1,56 @@
+import {defineStore} from 'pinia';
+import {ref} from 'vue';
+import type {MarketNews} from '@/types/marketNewsTypes';
+import {marketNewsApi} from "@/api/marketNewsApi";
+import {signalRService} from '@/api/signalRApi';
+
+export const useMarketNewsStore = defineStore('market-news', () => {
+    const news = ref<MarketNews[]>([]);
+    const coinSymbol = ref("");
+    const isLoading = ref(false);
+    const errorMessage = ref('');
+
+    const fetchRecentNews = async () => {
+        isLoading.value = true;
+        try {
+            news.value = await marketNewsApi.getRecentNews();
+            coinSymbol.value = "";
+        } catch (error) {
+            errorMessage.value = error.message;
+        } finally {
+            isLoading.value = false;
+        }
+    };
+
+    const fetchNewsBySymbol = async (symbol: string) => {
+        isLoading.value = true;
+        try {
+            news.value = await marketNewsApi.getNewsByCoin(symbol);
+            coinSymbol.value = symbol;
+        } catch (error) {
+            errorMessage.value = error.message;
+        } finally {
+            isLoading.value = false;
+        }
+    };
+
+    const initPortfolioSignalR = async () => {
+        signalRService.on('PublishNews', (data: MarketNews) => {
+            if (coinSymbol.value === "") {
+                news.value.unshift(data);
+            }
+            else if (data.relatedSymbols.includes(coinSymbol.value)) {
+                news.value.unshift(data);
+            }
+        });
+    };
+
+    return {
+        news,
+        isLoading,
+        errorMessage,
+        fetchRecentNews,
+        fetchNewsBySymbol,
+        initPortfolioSignalR
+    }
+});
